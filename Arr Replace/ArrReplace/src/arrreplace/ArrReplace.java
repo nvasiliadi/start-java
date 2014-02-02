ed08ab0dc462113d5354b3aee24379f2cf7b77ff
nvasiliadi <vasiliady.nikolay@gmail.com>
2014-02-02 13:19:11 +0200

    arrReplace 1 variant

---
+++ Arr Replace/ArrReplace/src/arrreplace/ArrReplace.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arrreplace;
+
+/**
+ *
+ * @author su
+ */
+public class ArrReplace {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int [] arr = {1,2,3,4,5,6,7,8,9,0};
+        for (int i=arr.length-1; i>=0; i--)
+        {
+            System.out.println(arr [i]);
+        }
+        
+    }
+    
+}
